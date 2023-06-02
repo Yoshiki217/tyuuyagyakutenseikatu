3e1f27abbb455394826f471b2a4d8634b2b13bef
LAPTOP-R2BG8LEC\2210134 <2210134@ecc.ac.jp>
2023-06-03 03:34:52 +0900

    add/footer.tsx,header.tsx layout.tsxで設定

---
+++ 2420/src/app/header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+const header = () => {
+  return (
+    <header className="z-50 w-full bg-white text-sm py-5 dark:bg-gray-800">
+        <div className='flex flex-wrap justify-center'>
+            <div className="flex items-center justify-between">
+                <a className="flex-none text-3xl font-semibold dark:text-white" href="#">SAMPLE</a>
+            </div>
+        </div>
+        <div className="flex flex-row items-end gap-5 mt-5 justify-end px-5 sm:mt-0 sm:pl-5">
+            <a className="justify-end font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Brand一覧</a>
+            <a className="justify-end font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">季節ごとのLOOK</a>
+            <a className="justify-end font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">検索</a>
+            <a className="justify-end font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">ブランドはこちら</a>
+            <a className="justify-end font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">マイページ</a>
+            <a className="justify-end font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Instagram</a>
+            
+        </div>
+    </header>
+  );
+};
+
+export default header;
--- 2420/src/app/layout.tsx
+++ 2420/src/app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
+import 'tailwindcss/tailwind.css'
 import { Inter } from 'next/font/google'
+import Header from './header';
+import Footer from './footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='bg-white'>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
