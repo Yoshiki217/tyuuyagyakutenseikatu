b3c61f939578b5258d7562d113f45bc1127677a2
tuzi <moku-moku.1208@i.softbank.jp>
2023-06-06 10:07:48 +0900

    amend page.tsx

--- 2420/src/app/page.tsx
+++ 2420/src/app/page.tsx
@@ -8,30 +8,35 @@ const Home: NextPage = () => {
       <div className="bg-white py-6 sm:py-8 lg:py-12">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
           <div className="mb-6 flex items-end justify-start gap-4">
+            
             <div>
-              <a href="https://homelesstailor.tumblr.com/post/186747414468/19aw" className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-                <Image src="/homelesstailor.png" width={334}height={500}alt="Homelestailor" className="wobject-cover object-center transition duration-200 group-hover:scale-110"/>
-                <span className="text-gray-500">Homelestailor  ルック</span>
-              </a>
               <div>
+                <a href="https://homelesstailor.tumblr.com/post/186747414468/19aw" className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
+                  <Image src="/homelesstailor.png" width={334}height={500}alt="Homelestailor" className="wobject-cover object-center transition duration-200 group-hover:scale-110"/>
+                  <span className="text-gray-500">Homelestailor  ルック</span>
+                </a>
+                
                 <a href="https://homelesstailor.tumblr.com/post/186747414468/19aw" className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
                   <Image src="/homelesstailor.png" width={334}height={500}alt="Homelestailor" className="object-cover object-center transition duration-200 group-hover:scale-110"/>
                   <span className="text-gray-500">Homelestailor  ルック</span>
                 </a>
               </div>
             </div>
+
             <div>
-              <a href="https://homelesstailor.tumblr.com/post/186747414468/19aw" className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-                <Image src="/homelesstailor.png" width={334}height={500}alt="Homelestailor" className="object-cover object-center transition duration-200 group-hover:scale-110"/>
-                <span className="text-gray-500">Homelestailor  ルック</span>
-              </a>
               <div>
+                <a href="https://homelesstailor.tumblr.com/post/186747414468/19aw" className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
+                  <Image src="/homelesstailor.png" width={334}height={500}alt="Homelestailor" className="object-cover object-center transition duration-200 group-hover:scale-110"/>
+                  <span className="text-gray-500">Homelestailor  ルック</span>
+                </a>
+
                 <a href="https://homelesstailor.tumblr.com/post/186747414468/19aw" className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
                   <Image src="/homelesstailor.png" width={334}height={500}alt="Homelestailor" className="object-cover object-center transition duration-200 group-hover:scale-110"/>
                   <span className="text-gray-500">Homelestailor  ルック</span>
                 </a>
               </div>
             </div>
+
           </div>
         </div>
       </div>    
