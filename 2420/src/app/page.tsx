3c6bf6cafd394c868898d51abed7427d004fe6d3
tuzi <moku-moku.1208@i.softbank.jp>
2023-06-06 09:30:06 +0900

    fordezain page.txt tailwind

--- 2420/src/app/page.tsx
+++ 2420/src/app/page.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 const Home: NextPage = () => {
   return (
     <>
-    
       <div className="bg-white py-6 sm:py-8 lg:py-12">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
           <div className="mb-6 flex items-end justify-start gap-4">
