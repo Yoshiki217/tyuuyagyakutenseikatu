69a826414481fbf180aa6e638f60520067450287
LAPTOP-R2BG8LEC\2210134 <2210134@ecc.ac.jp>
2023-06-06 10:37:41 +0900

    add/concept-sample

---
+++ 2420/src/app/concept/page.tsx
@@ -0,0 +1,22 @@
+import { Inter } from '@next/font/google'
+import { Montserrat } from "@next/font/google";
+
+const inter = Inter({variable: '--font-inter', subsets: ['latin'] })
+const montserrat = Montserrat({
+    subsets: ["latin"],
+    display: "swap",
+  });
+  
+export default function Concept() {
+  return (
+    <>
+    <div className={montserrat.className}>
+        <p className='flex justify-center text-center bg-white py-40 px-auto'>
+            stillness and motion , mininal and maximal , mode and tradition.
+            <br></br><br></br>
+            「無から有へ。そのはざまの部分を表現する。」
+        </p>
+    </div>
+    </>
+  )
+}
