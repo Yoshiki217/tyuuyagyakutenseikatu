3a6a0be21d8fbd1a35ef6c16c748d7485a975df2
LAPTOP-R2BG8LEC\2210134 <2210134@ecc.ac.jp>
2023-06-20 09:51:15 +0900

    fix/password隠す

--- 2420/src/app/userLogin/page.tsx
+++ 2420/src/app/userLogin/page.tsx
@@ -14,7 +14,7 @@ const userlogin = () => {
         
                 <div>
                     <label htmlFor="password" className="inline-block text-gray-800 text-sm sm:text-base mb-2">Password</label>
-                    <input name="password" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+                    <input type="password" name="password" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
                 </div>
     
                 <button className="block bg-gray-800 hover:bg-gray-700 active:bg-gray-600 focus-visible:ring ring-gray-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Log in</button>
